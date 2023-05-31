f64570514fd07074663050dd39d293ed15b6e90c
Isabel <isabel.lim09@rafflesgirlssch.edu.sg>
2023-05-31 13:41:15 +0800

    Initial Commit

---
+++ Groupwork/GroupworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupworkApp.swift
+//  Groupwork
+//
+//  Created by Isabel Lim on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GroupworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
